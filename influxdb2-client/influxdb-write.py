6a90fbbc831977aee2b6c8fe038dfa68fbe1c29e
Konrad Renis <konrad.renis@gmail.com>
2022-04-28 14:54:31 +0200

    update

--- influxdb2-client/influxdb-write.py
+++ influxdb2-client/influxdb-write.py
@@ -17,13 +17,9 @@ point = Point("mem1") \
 .field("wilgotnosc", 25) \
 .time(datetime.utcnow(), WritePrecision.US)
 print(datetime.utcnow())
-<<<<<<< HEAD
-#cos mojego
-=======
 
-#xxxxxxx
 
->>>>>>> 689e0c128cfee097208386a7219aa24a8cf3afb8
+# send
 write_api.write(bucket, org, point)
 
 
